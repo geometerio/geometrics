6153c523ffb1dba0acf8ec496c42aaced07c2756
Oliver Switzer <oliver@geometer.io>
2021-02-24 14:26:11 -0500

    Init

---
+++ lib/geometrics.ex
@@ -0,0 +1,18 @@
+defmodule Geometrics do
+  @moduledoc """
+  Documentation for `Geometrics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Geometrics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/geometrics_test.exs
@@ -0,0 +1,8 @@
+defmodule GeometricsTest do
+  use ExUnit.Case
+  doctest Geometrics
+
+  test "greets the world" do
+    assert Geometrics.hello() == :world
+  end
+end
