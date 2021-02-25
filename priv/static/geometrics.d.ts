34d824f294509c920a778f82e39ef632fe597762
Eric Saxby <sax@geometer.io>
2021-02-25 11:19:57 -0800

    Commit compiled assets

---
+++ priv/static/geometrics.d.ts
@@ -0,0 +1,5 @@
+import { WebTracerProvider } from '@opentelemetry/web';
+declare const tracerProvider: WebTracerProvider;
+declare const rootCtx: import("@opentelemetry/api").Context;
+export { tracerProvider, rootCtx };
+//# sourceMappingURL=geometrics.d.ts.map
