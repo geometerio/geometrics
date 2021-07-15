7f2fabc4d6eaaa7cbd16092b3cb0a84d55b77242
Eric Saxby <sax@geometer.io>
2021-07-14 17:02:15 -0700

    Set phoenix config in test to remove warning
    Signed-off-by: Austin Putman <austinp@geometer.io>
    Co-authored-by: Austin Putman <austinp@geometer.io>

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,7 @@ import Config
 config :geometrics, :ecto_prefix, []
 config :geometrics, :collector_endpoint, "http://localhost:55681/v1/trace"
 config :geometrics, :warn_on_no_trace_context, true
+
+if config_env() == :test do
+  config :phoenix, :json_library, Jason
+end
