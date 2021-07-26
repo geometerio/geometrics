ef18be5683b5197dbef8a459bb30042ff953b8f4
Eric Saxby <sax@geometer.io>
2021-07-26 10:19:07 -0700

    Bump to 0.2.0-rc.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Geometrics.MixProject do
   use Mix.Project
 
   @scm_url "https://github.com/geometerio/geometrics"
-  @version "0.2.0-rc.2"
+  @version "0.2.0-rc.3"
 
   def project do
     [
